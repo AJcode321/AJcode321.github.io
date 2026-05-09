10cf11dd5cde266dde555e801963d61ba104fc88
AJcode321 <ajesh.sk20@gmail.com>
2026-05-09 01:59:48 +0000

    my website

--- script.js
+++ script.js
@@ -2,5 +2,18 @@ function tester() {
   console.log("test");  
 }
 
-document.querySelector("p").style.color = "red";
+function colors()
+{
+  document.querySelector("h1").style.color = "red";
+}
+
+const button = document.getElementById("button");
+
+
+  button.onclick = () => { 
+     alert("you clicked the button"); 
+     document.body.style.backgroundColor = "blue"; 
+  }
+
+document.querySelector("p").style.color = "yellow";
 document.querySelector("#button").addEventListener("click", () => { tester() })
