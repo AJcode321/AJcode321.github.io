4772798937dd6590d02b4edbc70193efacde6c05
AJcode321 <ajesh.sk20@gmail.com>
2026-05-15 21:27:08 -0400

    Implement dark and light mode toggle functionality

--- script.js
+++ script.js
@@ -9,11 +9,19 @@ function colors()
 
 const button = document.getElementById("button");
 
-
-  button.onclick = () => { 
-     alert("you clicked the button"); 
-     document.body.style.backgroundColor = "blue"; 
+     
+     button.onclick = () => { 
+     alert("you turned on dark mode"); 
+     document.body.style.backgroundColor = "black"; 
+     document.body.style.textShadow = "-0.8px -0.8px 0 white, 0.8px -0.8px 0 white, -0.8px  0.8px 0 white, 0.8px  0.8px 0 white"
   }
 
-document.querySelector("p").style.color = "yellow";
+const button2 = document.getElementById("button2");
+
+     button2.onclick = () => {
+  alert("you turned on light mode");
+  document.body.style.backgroundColor = "white";
+  document.body.style.textShadow = "-1px -1px 0 black, 1px -1px 0 black, -1px  1px 0 black, 1px  1px 0 black"
+  }
+
 document.querySelector("#button").addEventListener("click", () => { tester() })
