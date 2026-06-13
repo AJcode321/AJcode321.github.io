4ebf3d91537ebf48f4e5116deb7c574dc181fd15
AJcode321 <ajesh.sk20@gmail.com>
2026-06-13 18:07:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
-function tester() {
-  console.log("test");  
-}
-
-function colors()
-{
-  document.querySelector("h1").style.color = "red";
-}
-
 const button = document.getElementById("button");
-
-     
-     button.onclick = () => { 
-     alert("you turned on dark mode"); 
-     document.body.style.backgroundColor = "black"; 
-     document.body.style.textShadow = "-0.8px -0.8px 0 white, 0.8px -0.8px 0 white, -0.8px  0.8px 0 white, 0.8px  0.8px 0 white"
-  }
-
 const button2 = document.getElementById("button2");
+// 'More about me' now navigates to a separate about.html page.
 
-     button2.onclick = () => {
-  alert("you turned on light mode");
+button.addEventListener("click", () => {
+  alert("You turned on dark mode");
+  document.body.style.backgroundColor = "black";
+  document.body.style.color = "white";
+  document.body.style.textShadow = "-0.8px -0.8px 0 white, 0.8px -0.8px 0 white, -0.8px 0.8px 0 white, 0.8px 0.8px 0 white";
+});
+
+button2.addEventListener("click", () => {
+  alert("You turned on light mode");
   document.body.style.backgroundColor = "white";
-  document.body.style.textShadow = "-1px -1px 0 black, 1px -1px 0 black, -1px  1px 0 black, 1px  1px 0 black"
-  }
+  document.body.style.color = "black";
+  document.body.style.textShadow = "-1px -1px 0 black, 1px -1px 0 black, -1px 1px 0 black, 1px 1px 0 black";
+});
 
-document.querySelector("#button").addEventListener("click", () => { tester() })
+// No client-side editor behavior needed on this page.
+
